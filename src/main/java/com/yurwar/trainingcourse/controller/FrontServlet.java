9c02b25a7dda4a749e6f15c4b0b82c0c0044f0ad
Yurii Matora <yura.matora11@gmail.com>
2019-07-25 00:36:27 +0300

    fix: remove unnecessary jsp files

--- src/main/java/com/yurwar/trainingcourse/controller/FrontServlet.java
+++ src/main/java/com/yurwar/trainingcourse/controller/FrontServlet.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class FrontServlet extends HttpServlet {
     private static final Logger log = LogManager.getLogger();
